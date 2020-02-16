a6baf94f929c7df43f00ebd26d0c3ebbc5dfe05c
maxsb007 <maxsb007@users.noreply.github.com>
2020-02-15 22:06:57 -0600

    Ejemplo

--- app/src/main/java/com/example/quizapp_pro/MainActivity.java
+++ app/src/main/java/com/example/quizapp_pro/MainActivity.java
@@ -1,23 +1,18 @@
 package com.example.quizapp_pro;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgTema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //imgTema = findViewById(R.id.imagen_portada);
-
-        //imgTema.setImageResource(R.drawable.ic_launcher_foreground);
 
         Toast.makeText(MainActivity.this, "Ejemplo", Toast.LENGTH_SHORT).show();
     }
