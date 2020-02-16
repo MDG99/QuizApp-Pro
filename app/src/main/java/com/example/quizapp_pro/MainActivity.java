9b6cfdbe3c22d92c093bc8124471e8c351602c4b
maxsb007 <maxsb007@users.noreply.github.com>
2020-02-15 22:48:50 -0600

    Ejemplo

--- app/src/main/java/com/example/quizapp_pro/MainActivity.java
+++ app/src/main/java/com/example/quizapp_pro/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.quizapp_pro;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPlay;
+    private Button btnConfig;
+    private Button btnPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnPlay = findViewById(R.id.play_button);
+        btnConfig = findViewById(R.id.options_button);
+        btnPoints = findViewById(R.id.points_button);
 
-        Toast.makeText(MainActivity.this, "Ejemplo", Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/com/example/quizapp_pro/OptionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizapp_pro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
