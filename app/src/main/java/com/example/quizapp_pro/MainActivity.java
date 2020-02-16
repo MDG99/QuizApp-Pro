22c858a156ddd7ef3ecfa0cd7eef88ec3c87cda1
maxsb007 <maxsb007@users.noreply.github.com>
2020-02-15 21:00:44 -0600

    Ejemplo

--- app/src/main/java/com/example/quizapp_pro/MainActivity.java
+++ app/src/main/java/com/example/quizapp_pro/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.quizapp_pro;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "Ejemplo", Toast.LENGTH_SHORT).show();
     }
 }
