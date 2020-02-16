70bc9be10c7eb280d530933130064ca988aab975
Ignacio Isaac <ignacioisaac30@gmail.com>
2020-02-15 19:55:26 -0600

    Commit de inicialización

---
+++ app/src/main/java/com/example/quizapp_pro/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizapp_pro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
