22fac5c4ad15b245bd9f053333fb79c1307d61b6
Calixto09 <60831808+Calixto09@users.noreply.github.com>
2020-02-16 17:50:28 -0600

    Acabo de crear la activty para el apartado de música

---
+++ app/src/main/java/com/example/quizapp_pro/Activity5Music.java
@@ -0,0 +1,14 @@
+package com.example.quizapp_pro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity5Music extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity5_music);
+    }
+}
