1d3020a734b60e6af43ff2cdaf8af0d75b0929d5
Calixto09 <60831808+Calixto09@users.noreply.github.com>
2020-02-16 14:43:50 -0600

    Le añadí la foto a la activity 4

--- app/src/main/java/com/example/quizapp_pro/Activity4.java
+++ app/src/main/java/com/example/quizapp_pro/Activity4.java
@@ -2,7 +2,9 @@ package com.example.quizapp_pro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Activity4 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Activity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+
+        ImageView ViewImage = findViewById(R.id.imageViewFondo);
+
+        //Añadir fondo a la pantalla
+        Drawable fondo = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            fondo = getResources().getDrawable(R.drawable.fondo4, null);
+        }
+        ViewImage.setImageDrawable(fondo);
+
     }
 }
