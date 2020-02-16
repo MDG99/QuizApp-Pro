70b7e33c1f0aafad4a4e99bfca3cea39b9bf7996
maxsb007 <maxsb007@users.noreply.github.com>
2020-02-16 13:33:55 -0600

    inicio de java optionsactivity

--- app/src/main/java/com/example/quizapp_pro/OptionsActivity.java
+++ app/src/main/java/com/example/quizapp_pro/OptionsActivity.java
@@ -1,14 +1,33 @@
 package com.example.quizapp_pro;
 
+import android.os.Bundle;
+import android.widget.CheckBox;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class OptionsActivity extends AppCompatActivity {
+
+    private CheckBox chkTodos;
+    private CheckBox chkArte;
+    private CheckBox chkGeografia;
+    private CheckBox chkFrases;
+    private CheckBox chkVideojuegos;
+    private CheckBox chkHistoria;
+    private CheckBox chkCultura;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        chkTodos = findViewById(R.id.todos_checkbox);
+        chkArte = findViewById(R.id.arte_checkbox);
+        chkGeografia = findViewById(R.id.geografia_checkbox);
+        chkFrases = findViewById(R.id.frases_checkbox);
+        chkVideojuegos = findViewById(R.id.videojuegos_checkbox);
+        chkHistoria = findViewById(R.id.historia_checkbox);
+        chkCultura = findViewById(R.id.cultura_checkbox);
+
     }
 }
