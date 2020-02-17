c37319ee5f6612dd57caf67a2d68fee81affcb9c
Ignacio Isaac <ignacioisaac30@gmail.com>
2020-02-17 11:00:15 -0600

    Ya debe de mostrar las respuestas de manera aleatoria, están guardas en un arreglo y las trampas se habilitan o deshabilitan según corresponda

--- app/src/main/java/com/example/quizapp_pro/Activity3.java
+++ app/src/main/java/com/example/quizapp_pro/Activity3.java
@@ -3,11 +3,14 @@ package com.example.quizapp_pro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Activity3 extends AppCompatActivity {
 
@@ -28,14 +31,13 @@ public class Activity3 extends AppCompatActivity {
     private int quantityAnswers;
     private int questionsForTopic;
     private int residueQuestionsForTopic;
+    private int cheatsQuantity;
     private int[] topicsToAsk;
 
     private boolean cheatsEnable;
     private boolean cheatRecorder;
 
     private List<Questions> questionsToShow;
-    private List<Answers> answersToShow;
-    private List<Answers> fakeAnswers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +58,7 @@ public class Activity3 extends AppCompatActivity {
         MainActivityViewModel model = new MainActivityViewModel();
         cheatRecorder = false;
         quantityAnswers = 4; //DEPENDE DE LA DIFICULTAD
+        int questionCurrent = 0;
 
 
         //Preguntas por tópico YA LO HACE OTRA CLASE
@@ -63,13 +66,86 @@ public class Activity3 extends AppCompatActivity {
 
         //Llenado de las preguntas por tópico
         questionsToShow.addAll(model.questionsByTopicRandom(questionsQuantity,topicsToAsk));
+        Questions[] questionsToShowSaved = new Questions[questionsToShow.toArray().length];
+        questionsToShow.toArray(questionsToShowSaved);
+
+        Answers[][] answersToShow = new Answers[questionsQuantity][difficult];
+
+        //Llenado de respuestas a mostrar aleatoriamente.- Te aseguras que esté la respuesta correcta
+        for(int i = 0; i<questionsQuantity;i++){
+            List<Answers> fakeAnswers = new ArrayList<>();
+            //Agregamos las respuestas falsas
+            fakeAnswers.add(questionsToShowSaved[i].getFakeAnswer1());
+            fakeAnswers.add(questionsToShowSaved[i].getFakeAnswer2());
+            fakeAnswers.add(questionsToShowSaved[i].getFakeAnswer3());
+            for (int d =0;d<difficult;d++){
+                if(d ==0)
+                answersToShow[i][d] = questionsToShowSaved[i].getCorrectAnswer();
+                else{
+                    Random rand = new Random();
+                    int aleatorio = rand.nextInt(fakeAnswers.size());
+                    answersToShow[i][d]= fakeAnswers.get(aleatorio);
+                    fakeAnswers.remove(aleatorio);
+                }
+            }
+        }
+
+        //Respuestas aleatorias
+        Answers[][] answersToShowSaved = new Answers[questionsQuantity][difficult];
+
+        for(int i = 0; i<questionsQuantity;i++){
+            List<Answers> auxAns = new ArrayList<>();
+            for (int h = 0; h < difficult; h++){
+                auxAns.add(answersToShow[i][h]);
+            }
+            for (int d =0;d<difficult;d++){
+                    Random rand = new Random();
+                    int aleatorio = rand.nextInt(auxAns.size());
+                    answersToShowSaved[i][d]= auxAns.get(aleatorio);
+                    auxAns.remove(aleatorio);
+            }
+        }
+
+        //Desplegar preguntas con sus debidas respuestas de manera aleatoria Tienes las que se van a mostrar, hay que cambiar el orden
+
+        questionsText.setText(questionsToShowSaved[questionCurrent].getQuestionText());
+
+        //Hay que hacer que se repita cuando se presiona un botón
+        switch (difficult){
+            case 2:
+                respuesta01.setText(answersToShowSaved[questionCurrent][0].getAnswerText());
+                respuesta02.setText(answersToShowSaved[questionCurrent][1].getAnswerText());
+                respuesta03.setEnabled(false);
+                respuesta03.setVisibility(View.INVISIBLE);
+                respuesta04.setEnabled(false);
+                respuesta04.setVisibility(View.INVISIBLE);
+                break;
+            case 3:
+                respuesta01.setText(answersToShowSaved[questionCurrent][0].getAnswerText());
+                respuesta02.setText(answersToShowSaved[questionCurrent][1].getAnswerText());
+                respuesta03.setText(answersToShowSaved[questionCurrent][2].getAnswerText());
+                respuesta04.setEnabled(false);
+                respuesta04.setVisibility(View.INVISIBLE);
+                break;
+            case 4:
+                respuesta01.setText(answersToShowSaved[questionCurrent][0].getAnswerText());
+                respuesta02.setText(answersToShowSaved[questionCurrent][1].getAnswerText());
+                respuesta03.setText(answersToShowSaved[questionCurrent][2].getAnswerText());
+                respuesta04.setText(answersToShowSaved[questionCurrent][3].getAnswerText());
+                break;
+        }
 
 
-        //Desplegar preguntas con sus debidas respuestas de manera aleatoria
 
         //Cambiar de pregunta
 
         //Habilitar o deshabilitar el contador de trampas
+        if(cheatsEnable){
+            cheatsCounter.setText(String.valueOf(cheatsQuantity));
+        }else{
+            cheatsCounter.setEnabled(false);
+            cheatsCounter.setVisibility(View.INVISIBLE);
+        }
 
         //Trampa: deshabilitar una respuesta incorrecta de manera aleatoria
 
