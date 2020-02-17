47e00d0f36f90b02c4a9dc29fac437c0977155dc
maxsb007 <maxsb007@users.noreply.github.com>
2020-02-16 18:52:37 -0600

    inicio de java optionsactivity

--- app/src/main/java/com/example/quizapp_pro/OptionsActivity.java
+++ app/src/main/java/com/example/quizapp_pro/OptionsActivity.java
@@ -2,6 +2,10 @@ package com.example.quizapp_pro;
 
 import android.os.Bundle;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +18,14 @@ public class OptionsActivity extends AppCompatActivity {
     private CheckBox chkVideojuegos;
     private CheckBox chkHistoria;
     private CheckBox chkCultura;
-
+    private Spinner preguntasSpinner;
+    private RadioButton btnFacil;
+    private RadioButton btnMedio;
+    private RadioButton btnDificil;
+    private Switch pistaSwitch;
+    private Spinner pistasSpinner;
+    private Topics[] topicsArray;
+    private boolean[] topicsChosen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,48 @@ public class OptionsActivity extends AppCompatActivity {
         chkVideojuegos = findViewById(R.id.videojuegos_checkbox);
         chkHistoria = findViewById(R.id.historia_checkbox);
         chkCultura = findViewById(R.id.cultura_checkbox);
+
+        preguntasSpinner = findViewById(R.id.preguntas_spinner);
+        btnFacil = findViewById(R.id.facil_radio);
+        btnMedio = findViewById(R.id.medio_radio);
+        btnDificil = findViewById(R.id.dificil_radio);
+        pistaSwitch = findViewById(R.id.pistas_button);
+        pistasSpinner = findViewById(R.id.pistas_spinner);
+
+
+        topicsArray = new Topics[]{
+                new Topics(0, "Arte"),
+                new Topics(1, "Geografía"),
+                new Topics(2, "Frases célebres"),
+                new Topics(3, "Videojuegos"),
+                new Topics(4, "Historia"),
+                new Topics(5, "Cultura general")
+        };
+
+        final CheckBox[] checkBoxes = {chkArte, chkGeografia, chkFrases, chkVideojuegos, chkHistoria, chkCultura};
+
+        topicsChosen = new boolean[]{false, false, false, false, false, false};
+
+        CompoundButton.OnCheckedChangeListener chkListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int i = 0;
+                for (CheckBox chk : checkBoxes) {
+                    if (chk.isChecked()) {
+                        topicsChosen[i] = true;
+                    }
+                    i++;
+                }
+
+            }
+        };
+
+        chkArte.setOnCheckedChangeListener(chkListener);
+        chkGeografia.setOnCheckedChangeListener(chkListener);
+        chkFrases.setOnCheckedChangeListener(chkListener);
+        chkVideojuegos.setOnCheckedChangeListener(chkListener);
+        chkHistoria.setOnCheckedChangeListener(chkListener);
+        chkCultura.setOnCheckedChangeListener(chkListener);
 
     }
 }
