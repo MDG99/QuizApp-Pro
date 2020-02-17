0631b345a2e135fd9c11826407715d79cddd89a6
maxsb007 <maxsb007@users.noreply.github.com>
2020-02-16 23:25:16 -0600

    mas avances pasar datos desde settings

--- app/src/main/java/com/example/quizapp_pro/MainActivity.java
+++ app/src/main/java/com/example/quizapp_pro/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnPlay;
     private Button btnConfig;
     private Button btnPoints;
+    private Button btnMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
                 startActivity(intent);
+
+
+            }
+        });
+
+        btnPoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity4.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        btnMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity5Music.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "LA APP CRASHEA", Toast.LENGTH_SHORT).show();
 
 
             }
